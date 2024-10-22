a7d44508355a922ea1201b1bd7131bea8b774d1d
ashik13en <ashikurrahman13en@gmail.com>
2024-10-22 17:09:06 +0600

    added react helmet

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,11 +3,15 @@ import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import ChefService from "../ChefService/ChefService";
 import Menu from "../Menu/Menu";
+import { Helmet } from "react-helmet-async";
 // import img1 from "../../../assets/menu/salad-bg.jpg";
 
 const Home = () => {
   return (
     <div className="">
+      <Helmet>
+        <title>Bistro | Home</title>
+      </Helmet>
       <Banner />
       <div className="p-8">
         <Category />
